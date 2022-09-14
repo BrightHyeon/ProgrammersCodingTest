0e12d886663cdc31cd6bd41fec561e9c22e05de7
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-09-14 14:17:55 +0900

    [Add] level2 멀리 뛰기 - pass

--- Level2/멀리 뛰기.swift
+++ Level2/멀리 뛰기.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func solution2(_ n:Int) -> Int {
+    if n == 1 {
+        return 1
+    } else if n == 2 {
+        return 2
+    } else {
+        return increase(n: n)
+    }
+}
+
+func increase(n: Int) -> Int {
+    var base: [Int] = [1,2]
+    for _ in 3...n {
+        base.append(base[base.count-2]%1234567+base.last!%1234567)
+    }
+    return base.last!%1234567
+}
--- Level2/올바른 괄호.swift
+++ Level2/올바른 괄호.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func solution(_ s: String) -> Bool {
+    var lCount = 0
+    var rCount = 0
+    
+    for i in s {
+        if rCount > lCount {
+            return false
+        } else {
+            if i == "(" {
+                lCount += 1
+            } else if i == ")" {
+                rCount += 1
+            }
+        }
+    }
+    return lCount == rCount
+}
