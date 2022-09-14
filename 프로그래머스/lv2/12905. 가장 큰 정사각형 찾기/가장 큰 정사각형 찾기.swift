3684ee8abe443b4f1798aa8700f24cad8df2f80b
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-09-14 14:32:43 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 225.56 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ board:[[Int]]) -> Int {
+  let row = board.count
+  let column = board[0].count
+  var rect = board
+  
+  for i in 1..<row {
+    for j in 1..<column {
+      if rect[i][j] != 0 {
+        rect[i][j] = min(rect[i-1][j], rect[i][j-1], rect[i-1][j-1]) + 1
+      }
+    }
+  }
+  let width = rect.map{ $0.max()! }.max()!
+  return width * width
+}
