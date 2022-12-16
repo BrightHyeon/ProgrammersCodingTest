294e4718a800d3145ff62d1bd4cd800fb7b83893
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-12-16 18:30:34 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/lv0/120808. 분수의 덧셈/README.md
@@ -1,6 +1,6 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=swift) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.swift
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Darwin
 
 func solution(_ denum1:Int, _ num1:Int, _ denum2:Int, _ num2:Int) -> [Int] {
     let num = calLCM(num1: num1, num2: num2)
@@ -11,7 +12,7 @@ func solution(_ denum1:Int, _ num1:Int, _ denum2:Int, _ num2:Int) -> [Int] {
 func calLCM(num1: Int, num2: Int) -> Int {
     let ratio = Double(max(num1, num2)) / Double(min(num1, num2))
     var mul = 1.0
-    while round(ratio * mul * 1000000) / 1000000 != round(ratio * mul) {
+    while ratio * mul != floor(ratio * mul) {
         mul += 1
     }
     return max(num1, num2) * Int(mul)
@@ -19,7 +20,5 @@ func calLCM(num1: Int, num2: Int) -> Int {
 
 func calGCD(num1: Int, num2: Int) -> Int {
     let lcm = calLCM(num1: num1, num2: num2)
-    print(num1, num2)
-    print(num1*num2, lcm)
     return num1*num2 / lcm
 }
