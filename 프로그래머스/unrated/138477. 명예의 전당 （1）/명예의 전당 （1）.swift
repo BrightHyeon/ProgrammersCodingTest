569a257e8572453f82b54f2e20dbd1c6ed802b51
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-12-18 20:01:13 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.19 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ k:Int, _ score:[Int]) -> [Int] {
+    var awards = [Int]() {
+        didSet {
+            if awards.count > k {
+                awards = awards.sorted(by: { $0 > $1 })
+                awards.removeLast()
+            }
+        }
+    }
+    
+    var result = [Int]()
+    
+    for i in score {
+        awards.append(i)
+        result.append(awards.min()!)
+    }
+    
+    return result
+}
