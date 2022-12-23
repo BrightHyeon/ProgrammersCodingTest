edefcd6babeff906540f15fd99318ab7e542a407
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-12-23 19:37:02 +0900

    [level 3] Title: 베스트앨범, Time: 0.24 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.swift
@@ -1,15 +1,8 @@
-import Foundation
-
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    var result = [Int]()
-    
-    var musics: Dictionary<String, [(Int, idx: Int)]> = [:]
-    genres.enumerated().forEach {
-        musics[$1, default: []] += [(plays[$0], $0)]
+    let dict = genres.enumerated().reduce(into: [String: [(Int, idx: Int)]]()) {
+        $0[$1.element, default: []] += [(plays[$1.offset], $1.offset)]
     }
-    
-    let sorted = musics.sorted { $0.value.reduce(0) { $0 + $1.0 } > $1.value.reduce(0) { $0 + $1.0 } }
-    sorted.forEach { result += $0.value.sorted { $0.0 > $1.0 }.prefix(2).map { $0.idx } }
-    
-    return result
+    return dict
+        .sorted { $0.value.reduce(0) { $0 + $1.0 } > $1.value.reduce(0) { $0 + $1.0 } }
+        .reduce([Int]()) { $0 + $1.value.sorted { $0.0 > $1.0 }.prefix(2).map { $0.idx }}
 }
