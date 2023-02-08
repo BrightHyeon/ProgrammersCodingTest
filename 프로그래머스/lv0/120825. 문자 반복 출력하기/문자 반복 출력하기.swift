a09d8c1d6c44b6cd6dc19fe82f44a93283967424
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-02-08 16:44:37 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ my_string:String, _ n:Int) -> String {
+    var result = ""
+    
+    for i in my_string {
+        for _ in 1...n {
+            result += String(i)
+        }
+    }
+    
+    return result
+}
