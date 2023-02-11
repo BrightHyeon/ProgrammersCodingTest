d2ec0b886b418960cbc30cafa25f8849cca29f84
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-02-11 19:29:56 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.swift
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.swift
@@ -1,11 +1,5 @@
-import Foundation
-
 func solution(_ numbers:[Int], _ direction:String) -> [Int] {
     var arr = numbers
-    if direction == "left" {
-        arr.append(arr.removeFirst())
-    } else {
-        arr.insert(arr.removeLast(), at: 0)
-    }
+    direction == "left" ? arr.append(arr.removeFirst()) : arr.insert(arr.removeLast(), at: 0)
     return arr
 }
