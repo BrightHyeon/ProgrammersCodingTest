99aced5bd348f0d1ece77e5b041053de379e111a
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-02-14 19:10:42 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.16 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.swift
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.swift
@@ -1,5 +1,5 @@
 func solution(_ myString: String) -> [Int] {
     return myString.reduce([Int]()) {
-        Int(String($1)) != Optional(nil) ? ($0 + [Int(String($1))!]) : $0
+        $1.isNumber ? ($0 + [Int(String($1))!]) : $0
     }.sorted()
 }
