af98ccc9640674ce930fd6229221a132c8abe9e8
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-05-04 19:41:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 28 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -1,19 +1,21 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N = input[0], S = input[1]
-var arr = readLine()!.split(separator: " ").map { Int($0)! }
+var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+var res = 0
 
-var results: [[Int]] = []
-
-func dfs(_ sub: [Int], _ lastIdx: Int) {
-    if sub.reduce(0, +) == S { results.append(sub) }
-    guard lastIdx < N-1 else { return }
+func dfs(_ sum: Int, _ lastIdx: Int) {
+    if sum == S {
+        res += 1
+    } else if (sum > 0 && sum > S) || lastIdx >= N-1 {
+        return
+    }
     for i in lastIdx+1..<N {
-        dfs(sub + [arr[i]], i)
+        dfs(sum + arr[i], i)
     }
 }
 
 for (idx, i) in arr.enumerated() {
-    dfs([i], idx)
+    dfs(i, idx)
 }
 
-print(results.count)
+print(res)
