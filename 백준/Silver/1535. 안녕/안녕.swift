6fc2703b5252492933791ca686124f242a43799b
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-05-07 20:09:04 +0900

    [Silver II] Title: 안녕, Time: 36 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.swift
+++ 백준/Silver/1535. 안녕/안녕.swift
@@ -1,18 +1,16 @@
 let N = Int(readLine()!)!
 let h = readLine()!.split(separator: " ").map { Int($0)! }
 let p = readLine()!.split(separator: " ").map { Int($0)! }
-var people = [(Int, Int)](), res = 0
-
-for i in 0..<N {
-    people.append((h[i], p[i]))
-}
+var res = 0
 
 func dfs(_ limit: Int, _ sum: Int, _ idx: Int) {
-    guard limit < 100 else { return }
     res = max(res, sum)
-    guard idx < N else { return }
-    dfs(limit, sum, idx+1)
-    dfs(limit + people[idx].0, sum + people[idx].1, idx+1)
+    if idx < N {
+        dfs(limit, sum, idx+1)
+        if limit + h[idx] < 100 {
+            dfs(limit + h[idx], sum + p[idx], idx+1)
+        }
+    }
 }
 dfs(0, 0, 0)
 print(res)
