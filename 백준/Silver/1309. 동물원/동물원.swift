be939b99db546b825cd7f05b38f9a9cdad2cd5e1
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-05-10 20:11:02 +0900

    [Silver I] Title: 동물원, Time: 32 ms, Memory: 76088 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.swift
+++ 백준/Silver/1309. 동물원/동물원.swift
@@ -1,13 +1,14 @@
 let n = Int(readLine()!)!
+
 var dp = Array(repeating: [0, 0, 0], count: n)
 dp[0] = [1, 1, 1]
 
-for i in 1..<n {
-    dp[i][0] = dp[i-1][1] + dp[i-1][2]
-    dp[i][1] = dp[i-1][0] + dp[i-1][2]
-    dp[i][2] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+let mod = 9901
 
-    dp[i] = dp[i].map { $0 % 9901 }
+for i in 1..<n {
+    dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % mod
+    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod
+    dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % mod
 }
 
-print(dp[n-1].reduce(0, +) % 9901)
+print(dp[n-1].reduce(0, +) % mod)
