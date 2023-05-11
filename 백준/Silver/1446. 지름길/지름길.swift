5587e79f5c5bcdee98c17f81961339228db6662c
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-05-11 21:29:15 +0900

    [Silver I] Title: 지름길, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.swift
+++ 백준/Silver/1446. 지름길/지름길.swift
@@ -1,32 +1,23 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let n = input[0], d = input[1]
-var nodes: [Int] = []
-var arr = Array(repeating: Int.max, count: d+1)
-var dict: [Int: [(Int, Int)]] = [:]
-
-arr[0] = 0; arr[d] = d
+var nodes: [[Int]] = []
+var arr = Array(repeating: d, count: d+1)
 
 for _ in 1...n {
     let sc = readLine()!.split(separator: " ").map { Int($0)! }
     if sc[1]-sc[0] > sc[2], sc[1] <= d {
-        nodes.append(contentsOf: [sc[0], sc[1]])
+        nodes.append([sc[0], sc[1], sc[2]])
+        nodes.append([sc[1], sc[1], 0])
         arr[sc[0]] = sc[0]; arr[sc[1]] = sc[1]
-        dict[sc[0], default: []].append((sc[1], sc[2]))
     }
 }
-
-nodes = Array(Set(nodes)).sorted(by: { $0 < $1 })
-nodes.append(d)
+nodes.sort(by: { $0[0] < $1[0] })
+nodes.append([d, d, 0])
 
 var pre = 0
 for node in nodes {
-    arr[node] = min(arr[node], arr[pre] + node - pre)
-    if dict.keys.contains(node) {
-        for i in dict[node]! {
-            arr[i.0] = min(arr[i.0], arr[node] + i.1)
-        }
-    }
-    pre = node
+    arr[node[0]] = min(arr[node[0]], arr[pre] + node[0] - pre)
+    arr[node[1]] = min(arr[node[1]], arr[node[0]]+node[2])
+    pre = node[0]
 }
-
 print(arr[d])
