d209793851c25af252214c58b85ab341e5961e7e
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-05-19 18:49:05 +0900

    [Gold V] Title: Contact, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.swift
+++ 백준/Gold/1013. Contact/Contact.swift
@@ -1,57 +1,40 @@
-enum Cases {
-    case L
-    case M
-    case R
-}
+enum Cases { case L, M, R }
 
 func check(_ str: String) -> Bool {
-    let arr = Array(str)
-    var res = "" // 얘 count도 생각하길.
+    var res = ""
     var active = false
-    
     var pre = Cases.M, state = Cases.M
     
-    for i in arr {
+    for i in str {
         if !active {
             if state == .M {
-                // 1. 일단 start 지점만 생각.
                 if i == "0" {
-                    // 2. start + 0
                     pre = state
                     state = .R
                 } else {
-                    // 중립, 1 들어옴, pre가 L턴일 때,
                     if pre == .L {
-                        active = true // state는 여전히 중립인걸로.
+                        active = true
                     } else {
                         pre = state
                         state = .L
                     }
                 }
-            } else if state == .L {
-                if i == "0" {
-                    // L 상황일 때 0이 들어오는 건 ㄱㅊㄱㅊ
+            } else if state == .L && i == "1" {
+                if res.count >= 3 && res.suffix(2) == "00" {
+                    pre = state
+                    state = .M
                 } else {
-                    // 문제는 1이 들어올 때임. 될 수도 안될 수도~!
-                    if res.count >= 3 && res.suffix(2) == "00" {
-                        pre = state
-                        state = .M
-                    } else {
-                        return false
-                    }
+                    return false
                 }
             } else if state == .R {
                 if i == "0" {
-                    // R 진행 중에 0은 용납 x.
                     return false
                 } else {
-                    // 1 들어오면 이제 R 종료. 01 세트니까.
                     pre = state
                     state = .M
                 }
             }
         } else {
-            // active 상황일 때~!
             if i == "1" {
                 if res.suffix(2) == "00" {
                     active = false
@@ -64,16 +47,9 @@ func check(_ str: String) -> Bool {
                 }
             }
         }
-        
         res += String(i)
-        
     }
-    
-    if active {
-        return !(res.last! == "0")
-    } else {
-        return state == .M
-    }
+    return active ? !(res.last! == "0") : state == .M
 }
 
 for _ in 1...Int(readLine()!)! {
