430c6a825018d7c3033d46d53d1017aee7a39ed7
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-07-05 15:54:44 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.26 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.swift
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func solution(_ rank: [Int], _ attendance: [Bool]) -> Int {
-    let rank = rank.enumerated().map { ($0.element, $0.offset) }
-    let res = zip(rank, attendance).filter { $0.1 }.sorted { $0.0.0 < $1.0.0 }.map { $0.0.1 }
-    return res[0] * 10000 + res[1] * 100 + res[2]
+    let list = zip(rank, attendance).enumerated().filter { $0.element.1 }.sorted{ $0.element.0 < $1.element.0 }.map { $0.offset }
+    return 10000*list[0] + 100*list[1] + list[2]
 }
